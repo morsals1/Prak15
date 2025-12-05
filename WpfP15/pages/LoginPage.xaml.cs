fb098d4094cdabd37ce7908e9c65296800cdf56d
morsals1 <menshik202@gmail.com>
2025-12-05 13:58:34 +0500

    firstSteps

---
+++ WpfP15/pages/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfP15.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        bool isManager = false;
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            isManager = true;
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void VisitorButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage());
+        }
+    }
+}
